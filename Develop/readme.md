add6d7747081690a06200fc026f0d0c9f74a417a
Christyn Garcia <christynsandusky@gmail.com>
2021-07-16 16:08:03 -0500

    Editing licenses/badges

--- Develop/readme.md
+++ Develop/readme.md
@@ -1,22 +1,37 @@
-# sdfsdf
-[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-## Description
-sdfsdf
+# asrweerwr
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+## Description 
+weerwer
+
 ## Table of Contents
-[Installation](#Installation)  
-[Usage](#Usage)  
-[License](#License)  
-[Contributing](#Contribution-Guidelines)  
-[Tests](#How-To-Test)  
-[Questions](#Questions)  
+[Installation](#Installation)
+[Usage](#Usage)
+[License](#License)
+[Contributing](#Contribution-Guidelines)
+[Tests](#How-To-Test)
+[Questions](#Questions)
+
+
 ## Installation
-```sdfsdf```
+```weerwer```
+
+
 ## Usage
-sdfsdf
+werewr
+
+##License
+undefined
+undefined
+
+
 ## Contribution Guidelines
-sdfsdf
+werwer
+
+
 ## How to Test
-No
+wrwer
+
+
 ## Questions
-[github](https://github.com/sdfsfsdf)
-sdfsfdsdf  
+[github](https://github.com/werwr)
+wrwer  
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,15 +3,15 @@
 function renderLicenseBadge(license) {
     switch (license) {
         case "MIT":
-            return "https://img.shields.io/badge/License-MIT-yellow.svg"
+            return "https://img.shields.io/badge/License-MIT-yellow.svg";
         case "APACHE 2.0":
-            return "https://img.shields.io/badge/License-Apache%202.0-blue.svg"
+            return "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
         case "GPL 3.0":
-            return "https://img.shields.io/badge/License-GPLv3-blue.svg"
+            return "https://img.shields.io/badge/License-GPLv3-blue.svg";
         case "BSD 3":
-            return "https://img.shields.io/badge/License-BSD%203--Clause-blue.svg"
+            return "https://img.shields.io/badge/License-BSD%203--Clause-blue.svg";
         default:
-            return ""
+            return "";
     }
 }
 // TODO: Create a function that returns the license link
@@ -19,15 +19,15 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
     switch (license) {
         case "MIT":
-            return "https://opensource.org/licenses/MIT"
+            return "https://opensource.org/licenses/MIT";
         case "APACHE 2.0":
-            return "https://opensource.org/licenses/Apache-2.0"
+            return "https://opensource.org/licenses/Apache-2.0";
         case "GPL 3.0":
-            return "https://www.gnu.org/licenses/gpl-3.0"
+            return "https://www.gnu.org/licenses/gpl-3.0";
         case "BSD 3":
-            return "https://opensource.org/licenses/BSD-3-Clause"
+            return "https://opensource.org/licenses/BSD-3-Clause";
         default:
-            return ""
+            return "";
     }
 }
 // TODO: Create a function that returns the license section of README
@@ -39,23 +39,38 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
     return `# ${data.title}
 ${renderLicenseSection(data.license)}
-## Description
+## Description 
 ${data.description}
+
 ## Table of Contents
-[Installation](#Installation)  
-[Usage](#Usage)  
-[License](#License)  
-[Contributing](#Contribution-Guidelines)  
-[Tests](#How-To-Test)  
-[Questions](#Questions)  
+[Installation](#Installation)
+[Usage](#Usage)
+[License](#License)
+[Contributing](#Contribution-Guidelines)
+[Tests](#How-To-Test)
+[Questions](#Questions)
+
+
 ## Installation
 \`\`\`${data.install}\`\`\`
+
+
 ## Usage
 ${data.use}
+
+##License
+${data.renderLicenseBadge}
+${data.renderLicenseLink}
+
+
 ## Contribution Guidelines
 ${data.contribute}
+
+
 ## How to Test
 ${data.test}
+
+
 ## Questions
 [github](https://github.com/${data.github})
 ${data.email}  
